bc5178666c44f2c0f1c6dfadabda9858ba0cbbf4
mikitane <miika.luiro@etasku.fi>
2021-03-14 18:06:51 +0200

    Setup Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:shopping_lists_app/state/providers.dart';
 import 'package:shopping_lists_app/routes.dart';
@@ -6,7 +8,10 @@ import 'package:shopping_lists_app/l10n/localization.dart';
 import 'package:shopping_lists_app/theme.dart';
 import 'package:shopping_lists_app/screens/shopping_lists_screen.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('shoppingLists');
+  await Hive.openBox('products');
   runApp(MyApp());
 }
 
