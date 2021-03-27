76c799673634c1f2ea7e42413eb34951c649f3fd
mikitane <miika.luiro@etasku.fi>
2021-03-27 02:12:51 +0200

    Add ability to remove shopping lists

--- lib/widgets/shopping_lists/shopping_lists.dart
+++ lib/widgets/shopping_lists/shopping_lists.dart
@@ -3,18 +3,26 @@ import 'package:shopping_lists_app/data/models/shopping_list/shopping_list_model
 import 'package:shopping_lists_app/widgets/shopping_lists/shopping_list_item.dart';
 
 class ShoppingLists extends StatelessWidget {
-  ShoppingLists({ required this.shoppingLists });
+  ShoppingLists({
+    required this.shoppingLists,
+    required this.onShoppingListTapped,
+    required this.onShoppingListLongPress,
+  });
 
   final List<ShoppingListModel> shoppingLists;
+  final void Function(ShoppingListModel) onShoppingListTapped;
+  final void Function(ShoppingListModel) onShoppingListLongPress;
 
   @override
   Widget build(BuildContext context) {
-
     return ListView.separated(
       padding: EdgeInsets.symmetric(horizontal: 12),
       itemCount: shoppingLists.length,
       itemBuilder: (context, index) {
-        return ShoppingListItem(shoppingList: shoppingLists[index]);
+        return ShoppingListItem(
+            shoppingList: shoppingLists[index],
+            onShoppingListTapped: onShoppingListTapped,
+            onShoppingListLongPress: onShoppingListLongPress);
       },
       separatorBuilder: (BuildContext context, int index) => Container(
         width: double.infinity,
